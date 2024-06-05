e3807035e6cef3574a10fbf9caae4cb27b59ea5a
DoubleW2w <1049951363@qq.com>
2024-06-05 16:18:57 +0800

    ✨ feat: chapter-03 RPC服务核心注解的设计与实现

---
+++ small-rpc-annotation/src/main/java/com/doublew2w/rpc/annotation/RpcService.java
@@ -0,0 +1,29 @@
+package com.doublew2w.rpc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 服务提供者注解
+ *
+ * @author: DoubleW2w
+ * @date: 2024/6/5 14:35
+ * @project: small-rpc
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RpcService {
+  /** 接口的Class */
+  Class<?> interfaceClass() default void.class;
+
+  /** 接口的ClassName */
+  String interfaceClassName() default "";
+
+  /** 版本号 */
+  String version() default "1.0.0";
+
+  /** 服务分组，默认为空 */
+  String group() default "";
+}
